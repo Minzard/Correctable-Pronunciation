4438dc66ad6aa0bf6222afba162f798a580085f1
joseph704 <freelove54@naver.com>
2019-09-24 01:09:04 +0900

    로그인 자동로그인 현상 해결 및 안정화, 예외처리

--- Test3/ViewController.swift
+++ Test3/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by 차요셉 on 2019. 3. 20..
 //  Copyright © 2019년 차요셉. All rights reserved.
 //
-
 import UIKit
 import Firebase
 class ViewController: UIViewController { 
@@ -13,36 +12,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     
     @IBAction func signin(_ sender: Any) {
+        
+        if (self.email.text == "") {
+            print("아이디가 입력되지 않았습니다")
+            return
+        }
+        else {
         Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (authResult, error) in
             // ...
             
             if(error != nil) {
                 
                 
-            
-                
-                Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
-                    // ...
+                Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { [weak self] user, error in
+                    
+                    guard self != nil else { return }
+                    self!.performSegue(withIdentifier: "Home", sender: nil)
                 }
                 
             }else {
                 let alert = UIAlertController(title: "알림", message: "회원가입완료", preferredStyle: UIAlertController.Style.alert)
                 alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
+                self.performSegue(withIdentifier: "Home", sender: nil)
             }
+        }
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view, typically from a nib.
-        Auth.auth().addStateDidChangeListener { (user, err) in
-            if user != nil {
-                self.performSegue(withIdentifier: "Home", sender: nil)
-            }
-        }
     }
-
-
 }
-
